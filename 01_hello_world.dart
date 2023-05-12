e8e71574e28e1fcec5354f0872e8eaffb7091244
Paul Nina <paul.ninat@gmail.com>
2023-05-11 21:58:18 -0400

    Inicial commit

---
+++ 01_hello_world.dart
@@ -0,0 +1,10 @@
+void main(){
+  
+  //var myName = 'Paul N';
+  //late final myName; 
+  //String myName = 'Paul';
+  const myName = 'Paul';
+    print ('hola $myName');
+  print ('hola ${ myName.toUpperCase() }');
+  print ('hola ${ 1+1 }');
+}
